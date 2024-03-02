5d645dfc2935049066bc4c22077c8c21743ee6aa
Slava Pogosov <pogoslav1@gmail.com>
2024-03-03 01:10:36 +0300

    add send and db

---
+++ database/database.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+db_file = '/Users/vicedant/Desktop/MIREA_BOT/database/users.db'
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except sqlite3.Error as e:
+        print(e)
+    return conn
+
+def add_user(user_id):
+    conn = create_connection(db_file)
+    with conn:
+        sql = ''' INSERT INTO users(user_id) VALUES(?) ON CONFLICT(user_id) DO NOTHING; '''
+        cur = conn.cursor()
+        cur.execute(sql, (user_id,))
+        conn.commit()
+
+def create_users_table():
+    conn = create_connection(db_file)
+    if conn is not None:
+        create_table_sql = """CREATE TABLE IF NOT EXISTS users (
+                                  id integer PRIMARY KEY,
+                                  user_id integer NOT NULL UNIQUE
+                              );"""
+        try:
+            c = conn.cursor()
+            c.execute(create_table_sql)
+        except sqlite3.Error as e:
+            print(e)
+        finally:
+            conn.close()
+    else:
+        print("Ошибка! Не удалось создать подключение к базе данных.")
+
+def get_all_users():
+    conn = create_connection(db_file)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT user_id FROM users")
+        user_ids = cur.fetchall()
+        return [user_id[0] for user_id in user_ids]
--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import telebot
 from telebot import types
 import os
 from wrapper.log import log_command
+from database.database import db_file, create_connection, create_users_table, add_user, get_all_users
 
+import sqlite3
 os.chdir("/Users/vicedant/Desktop/MIREA_BOT/") 
-
-import pandas as pd
-
-bot = telebot.TeleBot("6630080242:AAF2fHCKMtoJn6t8UJNgOU6hHtzxZ8LQv_U")
+bot = telebot.TeleBot("token")
 
 @bot.message_handler(commands=['start']) 
 @log_command
 def start_message(message):
+    add_user(message.from_user.id)
     bot.send_message(
         message.chat.id,  
         "📌 Добро пожаловать, дорогие студенты группы ЭФБО-07-23! Я бот, созданный чтобы облегчить ваше взаимодействие и учебу. Вы можете использовать следующие команды: \n\n1️⃣/group - получить список группы"
@@ -25,5 +25,21 @@ def send_docx_message(message):
     with open(doc_path, 'rb') as doc:
         bot.send_document(message.chat.id, doc, caption="😁 Вот список всей группы:")
 
+@bot.message_handler(commands=['send'])
+def send_to_all(message):
+    ADMIN_ID = 1975353168
+    if message.from_user.id == ADMIN_ID:
+        text_to_send = message.text.replace('/send ', '', 1)
+        user_ids = get_all_users()
+        for user_id in user_ids:
+            try:
+                bot.send_message(user_id, text_to_send)
+            except Exception as e:
+                print(f"Не удалось отправить сообщение пользователю {user_id}: {e}")
+    else:
+        bot.reply_to(message, "⛔️ У вас нет доступа к админ-командам")
+
+
 print("Бот запущен и готов к работе. Ожидание команд...")
+create_users_table()
 bot.polling()
