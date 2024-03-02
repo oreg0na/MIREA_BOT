b8f4e2397e21d980faf8ad5cb1e305a52d7103f6
Slava Pogosov <pogoslav1@gmail.com>
2024-03-02 21:27:44 +0300

    add parse .docs

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import telebot
-import openpyxl
+# import openpyxl
+import docx2txt
 
+import pandas as pd
 from io import BytesIO
 
-bot = telebot.TeleBot("TOKEN")
+bot = telebot.TeleBot("6630080242:AAF2fHCKMtoJn6t8UJNgOU6hHtzxZ8LQv_U")
 
 @bot.message_handler(commands=['start']) 
 def start_message(message):
@@ -13,12 +15,20 @@ def start_message(message):
     )
 
 @bot.message_handler(commands=['group'])
-def send_group(message):
-    wb = openpyxl.load_workbook('resources/group_list.docs') 
-    sheet = wb.active
+def group_list(message):
+    doc_text = docx2txt.process("resources/group_list.docs")
+    lines = doc_text.split("\n")
+    data = []
+    for line in lines:
+        splits = line.split(":")
+        if len(splits) == 2:
+            name, group = splits 
+            data.append({"name": name.strip(), "group": group.strip()})
+    df = pd.DataFrame(data) 
     with BytesIO() as output:
-        wb.save(output)
+        with pd.ExcelWriter(output) as writer:
+            df.to_excel(writer)
         output.seek(0)
-        bot.send_document(message.chat.id, output, filename='resources/group_list.docs')
+        bot.send_document(message.chat.id, output, filename="resources/group_list.docs") 
 
 bot.polling()
