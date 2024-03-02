7f67d3d36f059d397cc8b8085d46d4ae049e4675
Slava Pogosov <pogoslav1@gmail.com>
2024-03-03 00:30:43 +0300

    update log, add username tag

--- logs/log.py
+++ logs/log.py
@@ -1,7 +1,9 @@
 def log_command(func):
     def wrapper(message):
         user_id = message.from_user.id
+        username = message.from_user.username
         command = message.text
-        print(f"Получена команда: {command} от пользователя с ID: {user_id}")
+        user_info = f"{user_id}, @{username}" if username else f"{user_id}"
+        print(f"Получена команда: {command} от пользователя с ID и ником: {user_info}")
         func(message)
     return wrapper
