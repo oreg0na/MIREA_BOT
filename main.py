2fa0400895130d61f0f2e1753642e2a14f033af4
Slava Pogosov <pogoslav1@gmail.com>
2024-03-03 16:09:49 +0300

    add ping cmd

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import telebot
 from telebot import types
 import os
 import time
+import subprocess
+import requests
 
 from wrapper.log import log_command
 from database.database import create_users_table, add_user, get_all_users
@@ -10,13 +12,23 @@ from config.config import *
 os.chdir(path_file) 
 bot = telebot.TeleBot(token)
 
+
+def get_ip_info(ip_address):
+    url = f"https://ipinfo.io/{ip_address}/json"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+
 @bot.message_handler(commands=['start']) 
 @log_command
 def start_message(message):
     add_user(message.from_user.id)
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn_group = types.KeyboardButton("Список группы")
-    markup.add(btn_group)
+    btn_ping = types.KeyboardButton("Узнать свой пинг")
+    markup.add(btn_group, btn_ping)
 
     bot.send_message(
         message.chat.id,  
@@ -52,6 +64,30 @@ def send_to_all(message):
                 print(f"Не удалось отправить сообщение пользователю {user_id}: {e}")
     else:
         bot.reply_to(message, "⛔️ У вас нет доступа к админ-командам")
+
+@bot.message_handler(commands=['ping'])
+def ping(message):
+    bot.send_message(message.chat.id, "Введите IP-адрес, на который нужно выполнить ping:")
+    bot.register_next_step_handler(message, process_ping)
+
+def process_ping(message):
+    ip_address = message.text
+    try:
+        output = subprocess.check_output(['ping', '-c', '3', ip_address])
+        ip_info = get_ip_info(ip_address)
+        response_text = f"Пинг до {ip_address}:\n\n{output.decode('utf-8')}\n\n"
+        response_text += f"Информация об IP-адресе:\nСтрана: {ip_info.get('country', 'неизвестно')}\nГород: {ip_info.get('city', 'неизвестно')}"
+        bot.send_message(message.chat.id, response_text)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Ошибка при выполнении ping: {e}")
+
+
+@bot.message_handler(func=lambda message: message.text == "Узнать свой пинг")
+def handle_ping_button(message):
+    sent_message = bot.send_message(message.chat.id, "Вызываю команду /ping...")
+    time.sleep(1)
+    bot.delete_message(message.chat.id, sent_message.message_id)
+    ping(message)
 
 
 print("Бот запущен и готов к работе. Ожидание команд...")
