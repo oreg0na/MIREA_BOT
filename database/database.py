f520aefbf4c15f2d9020b88ccb3890f9abcc88a4
Slava Pogosov <pogoslav1@gmail.com>
2024-03-04 23:24:29 +0300

    update

--- database/database.py
+++ database/database.py
@@ -23,8 +23,10 @@ def create_users_table():
     conn = create_connection(db_file_user)
     if conn is not None:
         create_table_sql = """CREATE TABLE IF NOT EXISTS users (
+                                  keyboard_sent INTEGER DEFAULT 0,
                                   id integer PRIMARY KEY,
-                                  user_id integer NOT NULL UNIQUE
+                                  user_id integer NOT NULL UNIQUE,
+                                  keyboard_sent INTEGER DEFAULT 0
                               );"""
         try:
             c = conn.cursor()
@@ -43,3 +45,23 @@ def get_all_users():
         cur.execute("SELECT user_id FROM users")
         user_ids = cur.fetchall()
         return [user_id[0] for user_id in user_ids]
+def set_keyboard_sent(user_id, sent_status):
+    conn = create_connection(db_file_user)
+    with conn:
+        sql = 'UPDATE users SET keyboard_sent = ? WHERE user_id = ?;'
+        cur = conn.cursor()
+        cur.execute(sql, (sent_status, user_id,))
+        conn.commit()
+
+def is_keyboard_sent(user_id):
+    conn = create_connection(db_file_user)
+    with conn:
+        sql = 'SELECT keyboard_sent FROM users WHERE user_id = ?;'
+        cur = conn.cursor()
+        cur.execute(sql, (user_id,))
+        result = cur.fetchone()
+        if result:
+            return result[0] == 1
+        else:
+            add_user(user_id)
+            return False
