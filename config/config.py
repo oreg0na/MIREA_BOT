79f599569ae2b9c235c0103a794ab9d44a5a278b
Slava Pogosov <pogoslav1@gmail.com>
2024-03-04 11:12:50 +0300

    update

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-token = "7166138226:AAHsX_pkvxOtQxFYJmLYXN-7xijPY43llVM"
+token = "token"
 path_file = "/Users/vicedant/Desktop/MIREA_BOT/"
 path_db_users = '/Users/vicedant/Desktop/MIREA_BOT/database/users.db'
 path_db_passwords = '/Users/vicedant/Desktop/MIREA_BOT/database/passwords.db'
