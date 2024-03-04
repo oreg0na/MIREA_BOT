f106abc1d4cb843856d5405614344d564749b7e4
Slava Pogosov <pogoslav1@gmail.com>
2024-03-04 20:34:37 +0300

    start defevlop schedule

--- config/config.py
+++ config/config.py
@@ -1,6 +1,7 @@
 path_file = "/Users/vicedant/Desktop/MIREA_BOT/"
 path_db_users = '/Users/vicedant/Desktop/MIREA_BOT/database/db/users.db'
 path_db_passwords = '/Users/vicedant/Desktop/MIREA_BOT/database/db/passwords.db'
+path_db_schedule = '/Users/vicedant/Desktop/MIREA_BOT/database/db/schedule.db'
 path_key = '/Users/vicedant/Desktop/MIREA_BOT/database/secret.key'
 path_token = '/Users/vicedant/Desktop/MIREA_BOT/config/token.json'
 admin = 1975353168
